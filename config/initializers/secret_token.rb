b6b74c575e8dd079313f87b788f5a882f884e6e1
Will Kim <i.will.kim@gmail.com>
2012-04-30 19:57:00 -0600

    new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hello2::Application.config.secret_token = '3fec4806fca4c88737011457ef02babd1a4507484724c4ea75eb94692a39d18eba8b05a4749a450e88cc4efe0f1a7233ec15670f1815b846d1d2536512bc3556'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hello2::Application.config.session_store :cookie_store, key: '_hello2_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hello2::Application.config.session_store :active_record_store
